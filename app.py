13f098035635a772a0ffdf5db4025cf817172ba2
head47 <63517545+head47@users.noreply.github.com>
2023-02-17 15:26:08 +0300

    move index to /profile and add redirect

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+    return redirect(url_for('profile'))
+
+@app.route('/profile')
+def profile():
     return render_template('index.html')
